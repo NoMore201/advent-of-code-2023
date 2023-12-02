460bb05f1f9fb97a21d3a0b61c778aa40b6ce8f8
Domenico Iezzi <domenico@iezzi.info>
2023-12-02 18:47:19 +0100

    Add placeholder test for day 3

---
+++ source/day3.cpp
@@ -0,0 +1,17 @@
+#include "day3.hpp"
+
+#include <string_view>
+
+namespace {
+
+} // anonymous namespace
+
+int AoC::day3_solution_part1(std::string_view input) {
+    (void)input;
+    return 0;
+}
+
+int AoC::day3_solution_part2(std::string_view input) {
+    (void)input;
+    return 0;
+}
---
+++ test/source/advent_tests_day3.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <day3.hpp>
+
+#include <string_view>
+
+namespace {
+
+constexpr std::string_view exercise_input{
+};
+
+} // anonymous namespace
+
+TEST(AdventOfCode, Day3Part1) {
+}
+
+TEST(AdventOfCode, Day3Part2) {
+}
