af60a79442228bda27e6bb4ebbe5d51376de9326
Domenico Iezzi <domenico@iezzi.info>
2023-12-05 21:22:36 +0100

    Update clang-format config and fix formatting

--- source/utils.hpp
+++ source/utils.hpp
@@ -13,89 +13,88 @@
 
 namespace Utils {
 
-    template<typename Container>
-    concept Iterable = requires(Container container){
-        requires std::forward_iterator<typename Container::iterator>;
-        container.begin();
-        container.end();
-    };
+template <typename Container>
+concept Iterable = requires(Container container) {
+    requires std::forward_iterator<typename Container::iterator>;
+    container.begin();
+    container.end();
+};
 
-    template<typename T>
-    std::set<T> find_common_items(const Iterable auto &first, const Iterable auto &second) {
-        std::set<T> result{};
-        std::for_each(first.begin(), first.end(), [&second, &result](const T &item) {
-            if (std::find(second.begin(), second.end(), item) != second.end()) {
-                result.insert(item);
-            }
-        });
-        return result;
-    }
+template <typename T> std::set<T> find_common_items(const Iterable auto &first, const Iterable auto &second) {
+    std::set<T> result{};
+    std::for_each(first.begin(), first.end(), [&second, &result](const T &item) {
+        if (std::find(second.begin(), second.end(), item) != second.end()) {
+            result.insert(item);
+        }
+    });
+    return result;
+}
 
-    template<typename T> requires std::integral<T>
-    struct ParseResult {
-        T final_number{};
-        std::ptrdiff_t iterator_offset{};
-    };
-
-    template<typename T>
+template <typename T>
     requires std::integral<T>
-    std::optional<ParseResult<T>> try_parse_number(std::string_view str) {
-        T final_number{};
-        const auto *begin = str.data();
-        const auto *end = &*str.end();
-        auto parse_result = std::from_chars(begin, end, final_number);
-        if (parse_result.ec == std::errc()) {
-            const auto pointer_offset = std::distance(begin, parse_result.ptr);
-            return {ParseResult<T>{final_number, pointer_offset}};
-        }
+struct ParseResult {
+    T final_number{};
+    std::ptrdiff_t iterator_offset{};
+};
 
-        return {};
+template <typename T>
+    requires std::integral<T>
+std::optional<ParseResult<T>> try_parse_number(std::string_view str) {
+    T final_number{};
+    const auto *begin = str.data();
+    const auto *end = &*str.end();
+    auto parse_result = std::from_chars(begin, end, final_number);
+    if (parse_result.ec == std::errc()) {
+        const auto pointer_offset = std::distance(begin, parse_result.ptr);
+        return {ParseResult<T>{final_number, pointer_offset}};
     }
 
-    std::vector<std::string_view> split(std::string_view str, char delim);
+    return {};
+}
 
-    std::vector<std::string_view> split(std::basic_string_view<char>::const_iterator begin,
-                                        std::basic_string_view<char>::const_iterator end, char delim);
+std::vector<std::string_view> split(std::string_view str, char delim);
 
-    template<typename I>
+std::vector<std::string_view> split(std::basic_string_view<char>::const_iterator begin,
+                                    std::basic_string_view<char>::const_iterator end, char delim);
+
+template <typename I>
     requires std::integral<I>
-    I parse_integer(std::basic_string_view<char>::const_iterator begin,
-                    std::basic_string_view<char>::const_iterator end) {
-        I integer{};
-        const auto *begin_ptr = &*begin;
-        const auto length = std::distance(begin, end);
+I parse_integer(std::basic_string_view<char>::const_iterator begin,
+                std::basic_string_view<char>::const_iterator end) {
+    I integer{};
+    const auto *begin_ptr = &*begin;
+    const auto length = std::distance(begin, end);
 
-        auto result = std::from_chars(begin_ptr, begin_ptr + length, integer);
-        if (result.ec != std::errc()) {
-            throw std::runtime_error("Error while parsing value from string");
-        }
-
-        return integer;
+    auto result = std::from_chars(begin_ptr, begin_ptr + length, integer);
+    if (result.ec != std::errc()) {
+        throw std::runtime_error("Error while parsing value from string");
     }
 
-    template<typename I>
+    return integer;
+}
+
+template <typename I>
     requires std::integral<I>
-    I parse_integer(std::string_view str) {
-        return parse_integer<I>(str.begin(), str.end());
-    }
+I parse_integer(std::string_view str) {
+    return parse_integer<I>(str.begin(), str.end());
+}
 
-    template<typename I>
+template <typename I>
     requires std::integral<I>
-    I parse_integer(std::basic_string<char>::const_iterator begin, std::basic_string<char>::const_iterator end) {
-        return parse_integer<I>(std::string_view{begin, end});
-    }
+I parse_integer(std::basic_string<char>::const_iterator begin, std::basic_string<char>::const_iterator end) {
+    return parse_integer<I>(std::string_view{begin, end});
+}
 
-    template<typename T>
-    std::stack<T> reverse_stack(std::stack<T> &original) {
-        // TODO: should the original be consumed ?
-        std::stack<T> reversed{};
-        const auto original_size = original.size();
-        for (std::stack<char>::size_type count = 0; count < original_size; count++) {
-            const auto top = original.top();
-            original.pop();
-            reversed.push(top);
-        }
-        return reversed;
+template <typename T> std::stack<T> reverse_stack(std::stack<T> &original) {
+    // TODO: should the original be consumed ?
+    std::stack<T> reversed{};
+    const auto original_size = original.size();
+    for (std::stack<char>::size_type count = 0; count < original_size; count++) {
+        const auto top = original.top();
+        original.pop();
+        reversed.push(top);
     }
+    return reversed;
+}
 
 } // namespace Utils
