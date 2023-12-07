23d3af4563cb92789794ef81aedf056e803fb88e
Domenico Iezzi <domenico@iezzi.info>
2023-12-07 11:52:19 +0100

    Implement day 6

--- source/day6.cpp
+++ source/day6.cpp
@@ -1,6 +1,82 @@
 #include "day6.hpp"
 #include "utils.hpp"
+#include <gsl/assert>
 
-std::size_t AoC::day6_solution_part1(std::string_view) { return 0; }
+namespace {
 
-std::size_t AoC::day6_solution_part2(std::string_view) { return 0; }
+using usize = std::size_t; // NOLINT(readability-identifier-naming)
+
+constexpr usize start_speed_mm_s = 0;
+constexpr usize speed_increase_per_ms_factor = 1; // each ms holding button, speed increases by x*factor mm/ms
+
+std::vector<usize> parse_integers_from_string(std::string_view line) {
+    std::vector<usize> numbers;
+    for (usize index = 0; index < line.size();) {
+        auto parse_result = Utils::try_parse_number<usize>(line.substr(index));
+        if (parse_result) {
+            numbers.push_back(parse_result->final_number);
+            index += static_cast<usize>(parse_result->iterator_offset);
+        } else {
+            index++;
+        }
+    }
+    return numbers;
+}
+
+usize parse_full_integer(std::string_view line) {
+    std::string line_without_spaces{};
+
+    std::copy_if(line.begin(), line.end(), std::back_inserter(line_without_spaces),
+                 [](char item) { return item != ' '; });
+    const auto line_split_by_colon = Utils::split(line_without_spaces, ':');
+    return Utils::parse_integer<usize>(line_split_by_colon[1]);
+}
+
+} // anonymous namespace
+
+std::size_t AoC::day6_solution_part1(std::string_view input) {
+    const auto lines = Utils::split(input, '\n');
+    const auto times = parse_integers_from_string(lines[0]);
+    const auto distances = parse_integers_from_string(lines[1]);
+
+    Ensures(times.size() == distances.size());
+
+    usize result{1};
+    for (usize race = 0; race < times.size(); race++) {
+        auto record = distances[race];
+        auto race_duration = times[race];
+        usize ways_we_could_win = 0;
+
+        for (usize hold_time = 0; hold_time <= race_duration; hold_time++) {
+            const auto speed = hold_time * speed_increase_per_ms_factor;
+            const auto time_left_for_running = race_duration - hold_time;
+            const auto distance_travelled = time_left_for_running * speed;
+            if (distance_travelled > record) {
+                ways_we_could_win++;
+            }
+        }
+
+        result *= ways_we_could_win;
+    }
+
+    return result;
+}
+
+std::size_t AoC::day6_solution_part2(std::string_view input) {
+    const auto lines = Utils::split(input, '\n');
+    auto race_duration = parse_full_integer(lines[0]);
+    auto record = parse_full_integer(lines[1]);
+
+    usize ways_we_could_win = 0;
+
+    for (usize hold_time = 0; hold_time <= race_duration; hold_time++) {
+        const auto speed = hold_time * speed_increase_per_ms_factor;
+        const auto time_left_for_running = race_duration - hold_time;
+        const auto distance_travelled = time_left_for_running * speed;
+        if (distance_travelled > record) {
+            ways_we_could_win++;
+        }
+    }
+
+    return ways_we_could_win;
+}
--- test/source/advent_tests_day6.cpp
+++ test/source/advent_tests_day6.cpp
@@ -7,17 +7,21 @@
 namespace {
 
 constexpr std::string_view exercise_input{
+    "Time:        41     96     88     94\n"
+    "Distance:   214   1789   1127   1055"
 };
 
 constexpr std::string_view example_input{
+    "Time:      7  15   30\n"
+    "Distance:  9  40  200"
 };
 
 } // anonymous namespace
 
 TEST(AdventOfCode, Day6Part1) {
-    // EXPECT_EQ(403695602, AoC::day5_solution_part1(exercise_input));
+    EXPECT_EQ(4811940, AoC::day6_solution_part1(exercise_input));
 }
 
 TEST(AdventOfCode, Day6Part2) {
-    // EXPECT_EQ(219529182, AoC::day5_solution_part2(exercise_input));
+    EXPECT_EQ(30077773, AoC::day6_solution_part2(exercise_input));
 }
