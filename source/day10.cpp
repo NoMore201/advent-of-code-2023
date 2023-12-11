7b9427d5eb63600b22c43dc202020e6b70b9f456
Domenico Iezzi <domenico@iezzi.info>
2023-12-11 12:18:20 +0100

    Add placeholder for day 10

---
+++ source/day10.cpp
@@ -0,0 +1,28 @@
+#include "day10.hpp"
+#include "string.hpp"
+
+
+#include <string_view>
+
+namespace {
+
+using Size = std::size_t;
+
+
+} // namespace
+
+int AoC::day10_solution_part1(std::string_view input) {
+    const auto lines = Utils::String::split(input, '\n');
+
+    int result{};
+
+    return result;
+}
+
+int AoC::day10_solution_part2(std::string_view input) {
+    const auto lines = Utils::String::split(input, '\n');
+
+    int result{};
+
+    return result;
+}
---
+++ test/source/advent_tests_day10.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include <day10.hpp>
+
+#include <string_view>
+
+namespace {
+
+constexpr std::string_view exercise_input{};
+
+constexpr std::string_view example_input{};
+
+} // anonymous namespace
+
+TEST(AdventOfCode, Day10Part1) {
+    //    EXPECT_EQ(123, AoC::day10_solution_part1(exercise_input));
+}
+
+TEST(AdventOfCode, Day10Part2) {
+    //    EXPECT_EQ(964, AoC::day10_solution_part2(exercise_input));
+}
